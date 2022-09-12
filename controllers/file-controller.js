1e42a097cec52de22781c9527341815b77abe27b
nuruddin <sg7720@gmail.com>
2022-09-12 18:57:00 +0300

    change file base url

--- controllers/file-controller.js
+++ controllers/file-controller.js
@@ -1,6 +1,6 @@
 const uploadFile = require("../middlewares/upload");
 const fs = require("fs");
-const baseUrl = "http://188.68.220.210/api/file/";
+const baseUrl = "http://188.68.220.24/api/file/";
 const { User } = require('../models');
 const upload = async (req, res) => {
   try {
